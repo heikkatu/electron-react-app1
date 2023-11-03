d1c213cd7df7db228ff648c986ed77da19bef969
Tuomas Heikka <45489186+TuomasHeikka@users.noreply.github.com>
2023-11-03 10:36:31 +0200

    Working left right audio

--- src/renderer/NewPage.tsx
+++ src/renderer/NewPage.tsx
@@ -16,15 +16,30 @@ const sound2 = new Howl({ src: [audioFile2] });
 function NewPage() {
   // State to track volume
   const [volume, setVolume] = useState(sound2.volume());
+  const [panning, setPanning] = useState(0); // 0 is centered, -1 is left, 1 is right
 
-  // Function to play the looping audio
-  const playAudio1 = () => {
-    sound1.play();
+  const playSound = (sound: Howl, position: number) => {
+    sound.stereo(position); // Set the stereo position
+    sound.play();
   };
 
-  // Function to play the second audio
-  const playAudio2 = () => {
-    sound2.play();
+  const playAudio1Left = () => playSound(sound1, -1);
+  const playAudio1Right = () => playSound(sound1, 1);
+  const playAudio2Left = () => playSound(sound2, -1);
+  const playAudio2Right = () => playSound(sound2, 1);
+
+  // Function to pan audio left
+  const panLeft = () => {
+    const newPanningValue = Math.max(panning - 0.1, -1); // assuming panning ranges from -1 (left) to 1 (right)
+    sound1.stereo(newPanningValue);
+    setPanning(newPanningValue);
+  };
+
+  // Function to pan audio right
+  const panRight = () => {
+    const newPanningValue = Math.min(panning + 0.1, 1);
+    sound1.stereo(newPanningValue);
+    setPanning(newPanningValue);
   };
 
   // Function to stop all audios
@@ -34,30 +49,37 @@ function NewPage() {
   };
   // Function to adjust volume
   const adjustVolume = (changeInDb: number) => {
-    let newVolume = sound2.volume() * 10 ** (changeInDb / 10);
+    let newVolume = volume * 10 ** (changeInDb / 20);
     newVolume = Math.max(0, Math.min(1, newVolume)); // Ensure volume is between 0 and 1
+    sound1.volume(newVolume);
     sound2.volume(newVolume);
     setVolume(newVolume); // Update state
   };
 
-  // Function to increase volume by 2 dB
-  const increaseVolume = () => {
-    adjustVolume(2);
-  };
+  const increaseVolume = () => adjustVolume(2);
+  const decreaseVolume = () => adjustVolume(-2);
 
-  // Function to decrease volume by 2 dB
-  const decreaseVolume = () => {
-    adjustVolume(-2);
+  // Define a function to get the panning text
+  const getPanningText = (panValue: number) => {
+    if (panValue === 0) return 'Center';
+    if (panValue < 0) return 'Left';
+    return 'Right';
   };
 
   return (
     <div>
       <h1>This is the New Page</h1>
-      <button type="button" onClick={playAudio1}>
-        Play Audio 1
+      <button type="button" onClick={playAudio1Left}>
+        Play Audio 1 Left
       </button>
-      <button type="button" onClick={playAudio2}>
-        Play Audio 2
+      <button type="button" onClick={playAudio1Right}>
+        Play Audio 1 Right
+      </button>
+      <button type="button" onClick={playAudio2Left}>
+        Play Audio 2 Left
+      </button>
+      <button type="button" onClick={playAudio2Right}>
+        Play Audio 2 Right
       </button>
       <button type="button" onClick={stopAudios}>
         Stop Audios
@@ -68,7 +90,17 @@ function NewPage() {
       <button type="button" onClick={decreaseVolume}>
         Decrease Volume
       </button>
-      <div>Current Volume: {Math.round(volume * 100)}%</div>
+      <button type="button" onClick={panLeft}>
+        Pan Left
+      </button>
+      <button type="button" onClick={panRight}>
+        Pan Right
+      </button>
+      <div>Current Volume: {volume} </div>
+      <div>
+        Current Panning:
+        {getPanningText(panning)}
+      </div>
       <Link to="/">
         <button type="button">Back to Main Page</button>
       </Link>
